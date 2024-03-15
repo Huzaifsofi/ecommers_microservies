8a3aff645b76dfab26ead19d663093110f5c6e05
Huzaifsofi <stylishcollection666@gmail.com>
2024-03-16 00:13:05 +0530

    added product

--- ecom/app/models.py
+++ ecom/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    img = models.ImageField()
+    current_price = models.IntegerField()
+    real_price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
